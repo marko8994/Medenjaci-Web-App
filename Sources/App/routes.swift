a985e3921cd6db8750dcb1bd85e1a1e0ff209586
Marko Mladenović <mmladenoviccloud@gmail.com>
2021-02-04 00:08:27 +0100

    MWA-1: Initial project setup (#2)
    - Add Lead as a package
    - Add leaf as template for rendering view
    - Remove unnecessary routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,13 +1,9 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req in
+    
+    app.get { req -> EventLoopFuture<View> in
         return req.view.render("index", ["title": "Hello Vapor!"])
-    }app.get { req in
-        return "It works!"
     }
-
-    app.get("hello") { req -> String in
-        return "Hello, world!"
-    }
+    
 }
